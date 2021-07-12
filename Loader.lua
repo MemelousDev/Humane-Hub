b2e94faa7c649c50f56afa3c3b82dfd7558750c2
MemelousDev <86919741+MemelousDev@users.noreply.github.com>
2021-07-12 19:38:40 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,3 +2,4 @@ if game.PlaceId == 155615604 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MemelousDev/Humane-Hub/main/Games/PrisonLife.lua",true))()
 else
     game:GetService("Players").LocalPlayer:Kick("The Game You Are Playing On Is Not Supported!")
+end
